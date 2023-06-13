d9e1c39848baa7cd7a75bd4cecee3151017d29c9
BabidCow <dwcowie0509@gmail.com>
2023-06-13 12:19:27 -0700

    nav bar started

---
+++ capstone/src/Components/Nav.js
@@ -0,0 +1,15 @@
+function Nav() {
+return (
+    <ul className="navbar">
+        <li><img src="Logo.svg" alt="logo" /></li>
+        <li>Home</li>
+        <li>About</li>
+        <li>Menu</li>
+        <li>Reservations</li>
+        <li>Order Online</li>
+        <li>Login</li>
+    </ul>
+)
+}
+
+export default Nav
