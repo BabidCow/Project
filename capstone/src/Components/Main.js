b030bd7aecd38c75c6f10885225d30598c7ab8bf
BabidCow <dwcowie0509@gmail.com>
2023-06-19 10:24:43 -0700

    styled heading and main elements

--- capstone/src/Components/Main.js
+++ capstone/src/Components/Main.js
@@ -1,5 +1,13 @@
 function Main() {
-
+return (
+    <main className="main">
+    <h1>Little Lemon</h1>
+    <h2>Chicago</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br></br>
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br></br></p>
+    <img src="restauranfood.jpg" height="400px" width="360px" />
+    </main>
+)
 }
 
 export default Main
