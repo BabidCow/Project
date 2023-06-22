b877507539e31991baf8eddba3c71fc2fef31e30
BabidCow <dwcowie0509@gmail.com>
2023-06-22 14:22:05 -0700

    changed p font and made all elements within main indent by 20% padding lefy

--- capstone/src/Components/Main.js
+++ capstone/src/Components/Main.js
@@ -3,7 +3,7 @@ return (
     <main className="main">
     <h1>Little Lemon</h1>
     <h2>Chicago</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br></br>
+    <p className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br></br>
         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br></br></p>
     <img src="restauranfood.jpg" height="400px" width="360px" />
     </main>
